b69ac84060d3b69b1fbc80ab88c4a90a5e1f4822
Andrew Yates <andrew@laneyates.com>
2022-01-24 16:36:46 -0500

    javascript modify tables

--- image.js
+++ image.js
@@ -1,8 +1,11 @@
-var dataUrl = 'data:image/gif;base64,R0lGODlhCwALAIAAAAAA3pn/ZiH5BAEAAAEALAAAAAALAAsAAAIUhA+hkcuO4lmNVindo7qyrIXiGBYAOw==';
+// var dataUrl = 'data:image/gif;base64,R0lGODlhCwALAIAAAAAA3pn/ZiH5BAEAAAEALAAAAAALAAsAAAIUhA+hkcuO4lmNVindo7qyrIXiGBYAOw==';
 
-imageFoo = document.createElement('img');
-imageFoo.src = dataUrl;
+// imageFoo = document.createElement('img');
+// imageFoo.src = dataUrl;
 
-var img = document.getElementById('jsim');  // Create new img element
-console.log(img)
-img.src = dataUrl
+// var img = document.getElementById('jsim');  // Create new img element
+// console.log(img)
+// img.src = dataUrl
+
+
+document.getElementById("b1").innerHTML = "after"
