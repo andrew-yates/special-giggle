cbac0f930eed6058fed5812451eabd62193c4b40
Andrew Yates <andrew@laneyates.com>
2022-01-24 16:10:30 -0500

    dynamic image

---
+++ image.js
@@ -0,0 +1,8 @@
+var dataUrl = 'data:image/gif;base64,R0lGODlhCwALAIAAAAAA3pn/ZiH5BAEAAAEALAAAAAALAAsAAAIUhA+hkcuO4lmNVindo7qyrIXiGBYAOw==';
+
+imageFoo = document.createElement('img');
+imageFoo.src = dataUrl;
+
+var img = document.getElementById('jsim');  // Create new img element
+console.log(img)
+img.src = dataUrl
